65ff29b4d4063fb83e85a33da680ad7a3f3391c7
zophion <zophion16@gmail.com>
2026-01-30 02:20:51 -0800

    first version of zophion

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Products from "../components/Products";
-import InquiryForm from "../InquiryForm";
-import Contact from "../components/Contact";
+// import InquiryForm from "../InquiryForm";
+// import Contact from "../components/Contact";
 
 function Home() {
   return (
